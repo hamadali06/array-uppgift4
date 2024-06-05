f2ecb7b862cfad23f5380617d4ce7f4373a327bc
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:11:53 +0200

    Add project files.

---
+++ array uppgift4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Ahmed", "Ameen", "Mohammed", "Ivan", "Håkon" };
+            string första = namn[0];
+            foreach (string n in namn)
+            {
+                if (string.Compare(n, första) < 0)
+                {
+                    första = n;
+                }
+            }
+            Console.WriteLine(första);
+        }
+    }
+}
